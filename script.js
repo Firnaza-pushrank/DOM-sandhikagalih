0b7d87c0878a2c04bacd679a5f3acafeff275dfd
firnaza <naza.belajar@gmail.com>
2021-03-11 23:25:12 +0700

    queryselector part

--- script.js
+++ script.js
@@ -9,3 +9,6 @@ const p = document.getElementsByTagName('p');
 for (let i = 0; i < p.length; i++){
     p[i].style.backgroundColor = "red";
 }
+
+const p4 =document.querySelector('#b p');
+p4.style.backgroundColor = "green";
